345913bc80ea4ff052e0f20db17b9d3c5ab5b80d
Augustine Ebeke <52254118+Augustineebeke@users.noreply.github.com>
2026-01-18 12:23:19 +0700

    Finished the getHumanChoice Function
    I have completed the getHumanChoice function and it's working
    perfectly well. I also declare the humanScore and ComputerScore
    variables in the global scope. I'm enjoying building the game.

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -1,1 +1,22 @@
-console.log("Hello! World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomChoice = Math.random();
+    if (randomChoice < 0.33) {
+        return "rock";
+    } else if (randomChoice < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }    
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter your choice!");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
