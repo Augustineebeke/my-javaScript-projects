435bd4c812fb4dca73dde7021d6792770150b718
Augustine Ebeke <52254118+Augustineebeke@users.noreply.github.com>
2026-01-19 12:20:57 +0700

    Completion of the Game Project
    I have completed the Rock, Paper, Scissors Game and everything
    is working perfectly well. I have played it several times and
    adjusted my prompt messages. It's fun to reach this far.

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
     let randomChoice = Math.random();
@@ -13,10 +12,65 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Please enter your choice!");
+    let humanChoice = prompt("Please enter your choice! Chose Rock, Paper, or Scissors to play the game");
     humanChoice = humanChoice.toLowerCase();
     return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("it's a tie!");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You Lose ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+// 3. Round 1
+  const humanSelection1 = getHumanChoice();
+  const computerSelection1 = getComputerChoice();
+  playRound(humanSelection1, computerSelection1);
+
+  // 3. Round 2
+  const humanSelection2 = getHumanChoice();
+  const computerSelection2 = getComputerChoice();
+  playRound(humanSelection2, computerSelection2);
+
+  // 3. Round 3
+  const humanSelection3 = getHumanChoice();
+  const computerSelection3 = getComputerChoice();
+  playRound(humanSelection3, computerSelection3);
+
+  // 3. Round 4
+  const humanSelection4 = getHumanChoice();
+  const computerSelection4 = getComputerChoice();
+  playRound(humanSelection4, computerSelection4);
+
+  // 3. Round 5
+  const humanSelection5 = getHumanChoice();
+  const computerSelection5 = getComputerChoice();
+  playRound(humanSelection5, computerSelection5);
+
+  if (humanScore > computerScore) {
+    console.log("Congratulations! You won the game!");
+  } else if (humanScore < computerScore) {
+    console.log("Game over! The computer won.");
+  } else {
+    console.log("The game ended in a draw.");
+  }
+  // Final Tally
+    console.log("Final Scores - Your Score is:" + humanScore + " Computer score is:" + computerScore);
+}
+playGame();
